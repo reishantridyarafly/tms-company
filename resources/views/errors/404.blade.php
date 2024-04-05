f55c65cab08e3bda639882a709812195ebe524ae
Reishan10 <reishantridyarafly@gmail.com>
2024-04-06 06:18:41 +0700

    Feat error 404: Modify design for error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Landrick - Admin Dashboard Template</title>
+    <title>404 - TMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- favicon -->
