29fb93489de9d9178fb6df5d5209e33931c9baf7
Reishan10 <reishantridyarafly@gmail.com>
2024-04-06 06:17:52 +0700

    Feat error 404: Modify design for error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -37,14 +37,15 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-8 col-md-12 text-center">
                         <img src="{{ asset('assets') }}/images/404.svg" style="max-width: 500px;" alt="">
-                        <div class="text-uppercase mt-4 display-5 fw-semibold">Halaman tidak tersedia</div>
+                        <div class="text-uppercase mt-4 display-5 fw-semibold">Page Not Found</div>
                         <div class="text-capitalize text-dark mb-4 error-page"></div>
+                        <p class="text-muted para-desc mx-auto">Uh-oh! You've reached a digital dead end.</p>
                     </div><!--end col-->
                 </div><!--end row-->
 
                 <div class="row">
                     <div class="col-md-12 text-center">
-                        <a href="{{ route('beranda.index') }}" class="btn btn-primary mt-4">Kembali ke Beranda</a>
+                        <a href="{{ route('beranda.index') }}" class="btn btn-primary mt-4">Go To Beranda</a>
                     </div><!--end col-->
                 </div><!--end row-->
             </div><!--end container-->
